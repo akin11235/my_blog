6ecd807f5e9a534e43d7e7cef813046ac92e5f53
Joseph Oladunmoye <oladunmo@sheridancollege.ca>
2023-06-19 22:41:41 -0400

    create post model
    Created post model and customized django admin

---
+++ blog/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from . import models
+
+
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'author',
+        'created',
+        'updated',
+    )
+
+    list_filter = (
+        'status',
+    )
+
+    prepopulated_fields = {'slug': ('title',)}
+
+    search_fields = (
+        'title',
+        'author__username',
+        'author__first_name',
+        'author__last_name',
+    )
+
+
+admin.site.register(models.Post, PostAdmin)
